fcf8325b24bf5177aceed9b068d6217ea322751c
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2025-07-24 20:39:37 -0400

    redenrizar o html

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 #chamar a função Flask
 app = Flask(__name__)
 
 #criar uma rota
 @app.route('/home')
 def ola():
-    return '<h1>Bem vindo ao site Jogoteca</h1>'     
+    return render_template('lista.html')
 
 #rodar a aplicação
 app.run()
