2eee97aac1f7d23eb6591ee28f56d6226659354d
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2025-07-26 18:29:32 -0400

    teste com listas

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 #criar uma rota
 @app.route('/home')
 def ola():
-    return render_template('lista.html')
+    lista_de_jogos = ['God of War', 'Skyrim', 'Valorant']
+    return render_template('lista.html', titulo_home='Jogos',jogos = lista_de_jogos)     
 
 #rodar a aplicação
 app.run()
