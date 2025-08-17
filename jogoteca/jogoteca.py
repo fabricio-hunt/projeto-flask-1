028cf90f8dacd6f3436d270c8ea6ffb7f1907d65
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2025-08-17 17:53:13 -0400

    adicionar novos usuários

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -11,6 +11,24 @@ jogo3 = Jogo('Final Fantasy', 'RPG', 'SNES')
 jogo4 = Jogo('Mortal Kombat', 'Luta', 'SNES')
 lista_de_jogos = [jogo1, jogo2, jogo3, jogo4]
 
+#criar uma classe de usuários, para cadastrar novos usuários
+class Usuario:
+    def __init__(self, nome, nickname, senha):
+        self.nome = nome
+        self.nickname = nickname
+        self.senha = senha
+
+usuario1 = Usuario('João', 'joao1', 'senha1')
+usuario2 = Usuario('Maria', 'maria2', 'senha2')
+usuario3 = Usuario('José', 'jose3', 'senha3')
+
+#criar um dicionário de usuários
+usuarios = {
+    usuario1.nickname: usuario1,
+    usuario2.nickname: usuario2,    
+    usuario3.nickname: usuario3
+}
+
 app = Flask(__name__)
 app.secret_key = 'alura'
 
@@ -35,18 +53,20 @@ def criar():
     lista_de_jogos.append(jogo)
     return redirect(url_for('index'))
 @app.route('/login')
+
 def login():
     proxima = request.args.get('proxima')
     return render_template('login.html', proxima = proxima)
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    if 'olamundo' == request.form['senha']:
-        session['usuario'] = request.form['usuario']
-        flash(session['usuario'] + ' logado com sucesso!')
-        
-        proxima_pagina = request.form['proxima']
-        return redirect('/{}'.format(proxima_pagina))
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario'] = usuario.nickname
+            flash('Usuário logado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect('/{}'.format(proxima_pagina))
     else:
         flash('Usuário ou senha inválidos!')   
         return redirect(url_for('login'))
